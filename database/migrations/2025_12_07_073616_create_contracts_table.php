46b15ace37fd88c37e411ad52f0c1b50bd0d4956
Nyntdr <nayantandukar@gmail.com>
2026-02-12 12:36:28 +0545

    More fixes to contracts table

--- database/migrations/2025_12_07_073616_create_contracts_table.php
+++ database/migrations/2025_12_07_073616_create_contracts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\JobTitle;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,13 +15,14 @@ return new class extends Migration
         Schema::create('contracts', function (Blueprint $table) {
             $table->id('contract_id');
             $table->enum('contract_type',['full_time','part_time','intern']);
+            $table->string('job_title', 50)->default(JobTitle::INTERN->value);
             $table->date('start_date');
             $table->date('end_date')->nullable();
             $table->integer('probation_period')->default(0);
             $table->string('working_hours',100)->nullable();
             $table->decimal('salary',10,2);
             $table->enum('contract_status', ['active', 'expired', 'terminated', 'renewed'])->default('active');
-            
+
             $table->timestamps();
         });
     }
